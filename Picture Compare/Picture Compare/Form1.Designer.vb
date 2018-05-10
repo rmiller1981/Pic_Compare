ed76125564f2ff800d0d5a063da65c4d7cf9d912
Randy Miller <rmiller@onesourcestaffing.com>
2018-05-10 07:09:13 -0400

    Current Project
    Current project, I forget if its complete.

---
+++ Picture Compare/Picture Compare/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.progressOverall = New System.Windows.Forms.ProgressBar()
+        Me.ProgressFile = New System.Windows.Forms.ProgressBar()
+        Me.btnFiles = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnDirectory = New System.Windows.Forms.Button()
+        Me.pic1 = New System.Windows.Forms.PictureBox()
+        Me.pic2 = New System.Windows.Forms.PictureBox()
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(86, 124)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Processing images"
+        Me.Label1.Visible = False
+        '
+        'progressOverall
+        '
+        Me.progressOverall.Location = New System.Drawing.Point(6, 140)
+        Me.progressOverall.Name = "progressOverall"
+        Me.progressOverall.Size = New System.Drawing.Size(254, 23)
+        Me.progressOverall.TabIndex = 1
+        '
+        'ProgressFile
+        '
+        Me.ProgressFile.Location = New System.Drawing.Point(6, 169)
+        Me.ProgressFile.Name = "ProgressFile"
+        Me.ProgressFile.Size = New System.Drawing.Size(254, 23)
+        Me.ProgressFile.TabIndex = 2
+        '
+        'btnFiles
+        '
+        Me.btnFiles.Location = New System.Drawing.Point(29, 227)
+        Me.btnFiles.Name = "btnFiles"
+        Me.btnFiles.Size = New System.Drawing.Size(127, 23)
+        Me.btnFiles.TabIndex = 3
+        Me.btnFiles.Text = "Compare files"
+        Me.btnFiles.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(162, 212)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnDirectory
+        '
+        Me.btnDirectory.Location = New System.Drawing.Point(29, 198)
+        Me.btnDirectory.Name = "btnDirectory"
+        Me.btnDirectory.Size = New System.Drawing.Size(127, 23)
+        Me.btnDirectory.TabIndex = 5
+        Me.btnDirectory.Text = "Compare Directory files"
+        Me.btnDirectory.UseVisualStyleBackColor = True
+        '
+        'pic1
+        '
+        Me.pic1.Location = New System.Drawing.Point(12, 12)
+        Me.pic1.Name = "pic1"
+        Me.pic1.Size = New System.Drawing.Size(119, 109)
+        Me.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic1.TabIndex = 6
+        Me.pic1.TabStop = False
+        '
+        'pic2
+        '
+        Me.pic2.Location = New System.Drawing.Point(141, 12)
+        Me.pic2.Name = "pic2"
+        Me.pic2.Size = New System.Drawing.Size(119, 109)
+        Me.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic2.TabIndex = 7
+        Me.pic2.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(267, 262)
+        Me.Controls.Add(Me.pic2)
+        Me.Controls.Add(Me.pic1)
+        Me.Controls.Add(Me.btnDirectory)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnFiles)
+        Me.Controls.Add(Me.ProgressFile)
+        Me.Controls.Add(Me.progressOverall)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents progressOverall As System.Windows.Forms.ProgressBar
+    Friend WithEvents ProgressFile As System.Windows.Forms.ProgressBar
+    Friend WithEvents btnFiles As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnDirectory As System.Windows.Forms.Button
+    Friend WithEvents pic1 As System.Windows.Forms.PictureBox
+    Friend WithEvents pic2 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Picture Compare/Picture Compare/Form1.vb
@@ -0,0 +1,202 @@
+﻿Imports System.IO
+Imports System.ComponentModel
+
+Public Class Form1
+    Public worker As BackgroundWorker
+    Public worker2 As BackgroundWorker
+    Dim matchcount As Integer = 0
+    Dim mismatchcount As Integer = 0
+    Dim image1hold As Byte()
+    Dim file1 As String = "C:\test\1.jpg"
+    Dim file2 As String = "C:\test\2.jpg"
+    Dim filestatus() As Integer
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        progressOverall.Minimum = 0
+        worker = New System.ComponentModel.BackgroundWorker
+        worker.WorkerReportsProgress = True
+        worker.WorkerSupportsCancellation = True
+        AddHandler worker.DoWork, AddressOf worker_DoWork
+
+        worker2 = New System.ComponentModel.BackgroundWorker
+        worker2.WorkerReportsProgress = True
+        worker2.WorkerSupportsCancellation = True
+        AddHandler worker2.DoWork, AddressOf worker2_DoWork
+
+    End Sub
+
+    Private Sub worker_DoWork(ByVal sender As Object, ByVal e As System.ComponentModel.DoWorkEventArgs)
+        If worker.CancellationPending Then
+            Exit Sub
+        End If
+        pic1.Image = New Bitmap(file1)
+        pic2.Image = New Bitmap(file2)
+        compare(getImage(file1), getImage(file2))
+        Dim percent As Decimal = 0
+        percent = matchcount / image1hold.Length * 100
+        'filestatus(file2) = percent
+        MsgBox(percent & " Percent")
+        percent = 0
+        matchcount = 0
+        mismatchcount = 0
+        ' Seams like something is missing here
+
+    End Sub
+
+    Private Function getImage(ByVal imagefile As String) As Byte()
+        Dim bytes As Byte()
+        bytes = File.ReadAllBytes(imagefile)
+        SetFileProgressmax(bytes)
+        'SetFileProgress(ProgressFile, ProgressFile.Maximum)
+        Return bytes
+    End Function
+
+    Delegate Sub SetFileProgressmaxCallback(ByVal bytes As Byte())
+    Private Sub SetFileProgressmax(ByVal bytes As Byte())
+
+        'InvokeRequired required compares the thread ID of the
+        'calling thread to the thread ID of the creating thread.
+        'If these threads are different, it returns true.
+        If ProgressFile.InvokeRequired Then
+            Dim d As New SetFileProgressmaxCallback(AddressOf SetFileProgressmax)
+            Me.Invoke(d, New Object() {bytes})
+        Else
+            ProgressFile.Maximum = bytes.Length
+        End If
+    End Sub
+
+    Delegate Sub SetFileProgressCallback(ByVal bar As ProgressBar, ByVal value As Integer)
+    Private Sub SetFileProgress(ByVal bar As ProgressBar, ByVal value As Integer)
+
+        ' InvokeRequired required compares the thread ID of the
+        ' calling thread to the thread ID of the creating thread.
+        ' If these threads are different, it returns true.
+        If bar.InvokeRequired Then
+            Dim d As New SetFileProgressCallback(AddressOf SetFileProgress)
+            Me.Invoke(d, New Object() {bar, value})
+        Else
+            bar.Value = value
+        End If
+    End Sub
+
+    Private Sub compare(ByVal image1 As Byte(), ByVal image2 As Byte())
+        image1hold = image1
+        For count As Integer = 0 To image1.Length
+            If count = image1.Length Or count = image2.Length Then
+                Exit For
+            End If
+
+            If image1(count) = image2(count) Then
+                matchcount += 1
+            Else
+                mismatchcount += 1
+            End If
+            SetFileProgress(ProgressFile, ProgressFile.Value + 1)
+        Next
+        SetFileProgress(ProgressFile, ProgressFile.Maximum)
+    End Sub
+
+    Dim files As FileInfo()
+
+    Private Sub btnDirectory_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDirectory.Click
+        Dim FolderDialog As New FolderBrowserDialog
+        If FolderDialog.ShowDialog() = Windows.Forms.DialogResult.Cancel Then Exit Sub
+        Dim directory As New DirectoryInfo(FolderDialog.SelectedPath)
+        files = directory.GetFiles("*.jpg")
+        If files.Length < 2 Then Exit Sub
+        progressOverall.Maximum = files.Length
+        Label1.Visible = True
+        
+
+        worker2.RunWorkerAsync()
+    End Sub
+    Private Sub worker2_DoWork(ByVal sender As Object, ByVal e As System.ComponentModel.DoWorkEventArgs)
+        Dim filecount As Integer = 0
+        Dim filePosition As Integer = 1
+        Do
+            file1 = files(filecount).FullName
+            Do
+                file2 = files(filePosition).FullName
+                worker.RunWorkerAsync()
+                Do While worker.IsBusy
+
+                Loop
+                SetFileProgress(ProgressFile, 0)
+                filePosition += 1
+            Loop While filePosition < files.Length
+            'progressOverall.Value = filecount
+            SetFileProgress(progressOverall, progressOverall.Value + 1)
+            filecount += 1
+            filePosition = 0 + filecount
+        Loop While filecount < files.Length
+
+    End Sub
+
+    Private Sub btnFiles_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnFiles.Click
+        Dim filedialog As New OpenFileDialog
+        ' Select First File
+        filedialog.Title = "Please select first file to compare."
+        filedialog.InitialDirectory = "C:\test"
+        filedialog.ShowDialog()
+        file1 = filedialog.FileName
+        ' Select second file
+        filedialog.Title = "Please select second file to compare."
+        filedialog.ShowDialog()
+        file2 = filedialog.FileName
+        Label1.Visible = True
+        worker.RunWorkerAsync()
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        ProgressFile.Value = 0
+        progressOverall.Value = 0
+        Label1.Visible = False
+    End Sub
+End Class
+
+Class holder
+    Private Sub compare(ByVal image1 As String, ByVal image2 As String)
+        'Cursor = Cursors.WaitCursor
+        Application.DoEvents()
+
+        ' Load the images.
+        Dim bm1 As Bitmap = Image.FromFile(image1)
+        Dim bm2 As Bitmap = Image.FromFile(image2)
+
+        ' Make a difference image.
+        Dim wid As Integer = Math.Min(bm1.Width, bm2.Width)
+        Dim hgt As Integer = Math.Min(bm1.Height, bm2.Height)
+        Dim bm3 As New Bitmap(wid, hgt)
+
+        ' Create the difference image.
+        Dim are_identical As Boolean = True
+        'Dim r1, g1, b1, r2, g2, b2, r3, g3, b3 As Integer
+        Dim eq_color As Color = Color.White
+        Dim ne_color As Color = Color.Red
+        For x As Integer = 0 To wid - 1
+            For y As Integer = 0 To hgt - 1
+                If bm1.GetPixel(x, y).Equals(bm2.GetPixel(x, y)) Then
+                    bm3.SetPixel(x, y, eq_color)
+                Else
+                    bm3.SetPixel(x, y, ne_color)
+                    are_identical = False
+                End If
+            Next y
+        Next x
+
+        ' Display the result.
+        'Form1.picResult.Image = bm3
+
+        'Cursor = Cursors.Default
+        If (bm1.Width <> bm2.Width) OrElse (bm1.Height <> _
+            bm2.Height) Then are_identical = False
+        If are_identical Then
+            MessageBox.Show("The images are identical")
+        Else
+            MessageBox.Show("The images are different")
+        End If
+
+        bm1.Dispose()
+        bm2.Dispose()
+    End Sub
+End Class
